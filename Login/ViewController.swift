6347b3afb31129d04506ac040957b8f548a47ea5
Evgenii <emeleshkov@mail.ru>
2021-09-10 18:55:51 +0300

    begin

---
+++ Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Евгений Мелешков on 10.09.2021.
+//  Login screen test app.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
